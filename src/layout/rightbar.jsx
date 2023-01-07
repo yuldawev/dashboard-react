c2bd49bc119d451cf3b65dfc390e4e14bc2ee5db
GBehzod <mrgaybullayev@gmail.com>
2023-01-07 13:13:09 +0500

    somechanges

--- src/layout/rightbar.jsx
+++ src/layout/rightbar.jsx
@@ -1,12 +1,4 @@
-import {
-  MdAdd,
-  MdDarkMode,
-  MdLightMode,
-  MdLocalMall,
-  MdMenu,
-  MdPerson,
-  MdShoppingCart,
-} from "react-icons/md";
+import {MdAdd, MdDarkMode, MdLightMode, MdLocalMall, MdMenu, MdShoppingBag, MdShoppingCart,} from "react-icons/md";
 import RecentUpdate from "../components/Notification";
 import profile1 from "../assets/images/profile1.jpg";
 import profile2 from "../assets/images/profile2.jpg";
@@ -16,108 +8,104 @@ import Item from "../components/Item";
 
 
 const RightNavbar = () => {
-  return (
-    <div className="right">
-      <div className="top">
-        <div className="op">
-          <buttun id="menu-btn">
-            <MdMenu />
-          </buttun>
-          <div className="theme-toggler">
-            <div className="active">
-              <MdLightMode />
+    return (
+        <div className="right">
+            <div className="top">
+                <div className="op">
+                    <buttun id="menu-btn">
+                        <MdMenu/>
+                    </buttun>
+                    <div className="theme-toggler">
+                        <div className="active">
+                            <MdLightMode/>
+                        </div>
+                        <div>
+                            <MdDarkMode/>
+                        </div>
+                    </div>
+                    <div className="profile">
+                        <div className="info">
+                            <p>
+                                Hey, <b>Daniel</b>
+                            </p>
+                            <small className="text-muted">Admin</small>
+                        </div>
+                    </div>
+                    <div className="update">
+                        <div className="profile-photo">
+                            <img src={profile1} alt=""/>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-              <MdDarkMode />
+            <div className="on">
+                <div className="resent-updates">
+                    <h2>Recent Updated</h2>
+
+                    <RecentUpdate
+                        text="received this order of Night lion tech GPS drone"
+                        userName="Mike Tyson"
+                        time="2 Minutes Ago"
+                        img={profile2}
+                    />
+
+                    <RecentUpdate
+                        text="received this order of Night lion tech GPS drone"
+                        userName="Mike Tyson"
+                        time="2 Minutes Ago"
+                        img={profile3}
+                    />
+
+                    <RecentUpdate
+                        text="received this order of Night lion tech GPS drone"
+                        userName="Mike Tyson"
+                        time="2 Minutes Ago"
+                        img={profile4}
+                    />
+                </div>
             </div>
-          </div>
-          <div className="profile">
-            <div className="info">
-              <p>
-                Hey, <b>Daniel</b>
-              </p>
-              <small className="text-muted">Admin</small>
+
+            <div className="right">
+                <div className="sales-analiytics">
+                    <h2>Sales Analiytics</h2>
+
+
+                    <Item
+                        icon={MdShoppingCart}
+                        text="ONLINE ORDERS"
+                        time="Last 24 Hours"
+                        type="online"
+                        percent={37}
+                        number="3849"
+                    />
+
+                    <Item
+                        icon={MdLocalMall}
+                        type="offline"
+                        text="OFFLINE ORDERS"
+                        time="Last 24 Hours"
+                        percent={-17}
+                        number="1100"
+                    />
+
+                    <Item
+                        icon={MdShoppingBag}
+                        text="OFFLINE ORDERS"
+                        type="customer"
+                        time="Last 24 Hours"
+                        percent={25}
+                        number="849"
+                    />
+
+                    <Item
+                        icon={MdAdd}
+                        type="add-product"
+                        text="Add Product"
+                    />
+                </div>
             </div>
-          </div>
-          <div className="update">
-            <div className="profile-photo">
-              <img src={profile1} alt="" />
-            </div>
-          </div>
         </div>
-      </div>
-      <div className="on">
-        <div className="resent-updates">
-          <h2>Recent Updated</h2>
-
-          <RecentUpdate
-            text="received this order of Night lion tech GPS drone"
-            userName="Mike Tyson"
-            time="2 Minutes Ago"
-            img={profile2}
-          />
-
-          <RecentUpdate
-            text="received this order of Night lion tech GPS drone"
-            userName="Mike Tyson"
-            time="2 Minutes Ago"
-            img={profile3}
-          />
-
-          <RecentUpdate
-            text="received this order of Night lion tech GPS drone"
-            userName="Mike Tyson"
-            time="2 Minutes Ago"
-            img={profile4}
-          />
-        </div>
-      </div>
-
-      <div className="right">
-        <div className="sales-analiytics">
-          <h2>Sales Analiytics</h2>
-          
-
-          <Item
-          icon="MdShoppingCart"
-          name="ONLINE ORDERS"
-          text="Last 24 Hours"
-          success="-17%"
-          number="3849"
-          />
-
-          <Item
-          icon="MdLocalMall"
-          name="OFLINE ORDERS"
-          text="Last 24 Hours"
-          danger="-17%"
-          number="1100"
-          />
-
-          <Item
-          icon="MdShoppingBag"
-          name="OFFLINE ORDERS"
-          text="Last 24 Hours"
-          success="-17%"
-          number="3849"
-          />
-
-          <Item
-          
-          icon="MdAdd"
-          gap="Add Product"
-          />
-
-          <div className="iteam add-product"></div>
-          
-
-         
-
-          
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default RightNavbar;
